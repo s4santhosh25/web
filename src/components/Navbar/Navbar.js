5aff4fbdf865af3097be702de993986629a42a9d
s4santhosh25 <s4santhosh25@gmail.com>
2018-04-24 15:34:17 +0530

    Navbar title size changed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -25,13 +25,13 @@ class Navbar extends Component {
       return (
         <nav>
         <div className="nav-wrapper">
-          <a href="/" className="brand-logo">Logo</a>
+          <a href="/" className="brand-logo">ReactApp</a>
           <a href="/" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
           <ul className="right hide-on-med-and-down">
             <li><a href="sass.html">Sass</a></li>
             <li><a href="badges.html">Components</a></li>
             <li><a href="collapsible.html">Javascript</a></li>
-            <li><a href="mobile.html">Mobile</a></li>
+            <li><a href="collapsible.html"><i class="material-icons">arrow_upward</i></a></li>
           </ul>
           <ul className="side-nav" id="mobile-demo">
             <li><a href="sass.html">Sass</a></li>
