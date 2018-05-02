a121a0605d6b510b812e39487cc1d28644fb193f
s4santhosh25 <s4santhosh25@gmail.com>
2018-05-02 22:58:26 +0530

    added new page

--- src/components/ComponentA.js
+++ src/components/ComponentA.js
@@ -1,17 +1,59 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
+import toastr from 'toastr';
 
 class ComponentA extends Component {
-    componentWillMount() {
-        // this     .props     .verify();
+
+    constructor(props) {
+        super(props);
+        this.logout = this
+            .logout
+            .bind(this);
+
         console.log('ComponentA', this.props)
     }
+
+    logout() {
+        sessionStorage.removeItem('main.token');
+        this
+            .props
+            .history
+            .replace('/');
+        toastr.success('Logout Successfull');
+    }
+
     render() {
         return (
-            <div>A
-                <Link to='/b'>Go to B</Link>
-                <button onClick={this.props.verify}/>{this.props.result.status}
+            <div style={{
+                position: 'relative'
+            }}>
+                <div
+                    className='logout'
+                    style={{
+                    float: 'right',
+                    marginRight: '05px',
+                    width: '100px',
+                    textAlign: 'center',
+                    backgroundColor: 'transparent'
+                }}
+                    onClick={this.logout}>
+                    <li >
+                        <a
+                            className="waves-effect waves-light  waves-effect waves-light modal-trigger"
+                            style={{
+                            color: 'white'
+                        }}>
+                            <i className="material-icons right">directions</i>Logout</a>
+                    </li>
+                </div>
+                <div className="imageloader responsive-img"></div>
+                <div
+                    className="container"
+                    style={{
+                    marginTop: '0px',
+                    fontSize: '36px',
+                    textAlign: 'center'
+                }}>Welcome !</div>
             </div>
         );
     }
